ecec7b0d42984d895885a60dd3b33a30559045a6
Max-Mendes91 <maxmendesnoah1991@gmail.com>
2025-11-07 12:09:22 +0100

    chore: Remove pnpm workspace configuration and update type definitions for React in TypeScript files

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import { DayPicker } from "react-day-picker";
+import { DayPicker, type DayPickerProps } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+export type CalendarProps = DayPickerProps;
 
-function Calendar({
+export function Calendar({
   className,
   classNames,
   showOutsideDays = true,
@@ -51,14 +51,19 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
-      }}
+      components={
+        {
+          IconLeft: (props: React.SVGProps<SVGSVGElement>) => (
+            <ChevronLeft {...props} className="h-4 w-4" />
+          ),
+          IconRight: (props: React.SVGProps<SVGSVGElement>) => (
+            <ChevronRight {...props} className="h-4 w-4" />
+          ),
+        } as any
+      }
       {...props}
     />
   );
 }
-Calendar.displayName = "Calendar";
 
-export { Calendar };
+Calendar.displayName = "Calendar";
